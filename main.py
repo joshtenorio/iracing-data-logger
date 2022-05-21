d0ad6c7421f8a62e3c929e1eca790d6fc6213f6f
Joshua Tenorio <joshliam09@gmail.com>
2022-05-21 15:47:29 -0700

    estimating position

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 import pyqtgraph as pg
 import time
 import irsdk
+import math
 
 # this is our State class, with some helpful variables
 class State:
@@ -14,10 +15,10 @@ class State:
     last_car_setup_tick = -1
 
 win = pg.GraphicsLayoutWidget(show=True)
-win.setWindowTitle('speed')
+win.setWindowTitle('dataaa')
 
 f = open("data.csv", "w")
-f.write("Time (s),Laps (#),Speed (m/s),Gear (#),Brake (%),Throttle (%),SteeringAngle (rad)\n")
+f.write("Time (s),Laps (#),Speed (m/s),Gear (#),Brake (%),Throttle (%),Steering Angle (rad),X Position (m),Y Position (m)\n")
 f.close()
 
 # 1) Simplest approach -- update data in the array such that plot appears to scroll
@@ -28,37 +29,63 @@ data1 = np.random.normal(size=1500)
 curve2 = p2.plot(data1)
 ptr1 = 0
 
+win.nextRow()
+
+map: pg.PlotItem = win.addPlot()
+map.showGrid(x=True, y=True)
+
 # initializing ir and state
 ir = irsdk.IRSDK()
 ir.startup()
 state = State()
 
+xPos = 0
+yPos = 0
+
+arrX = []
+arrY = []
+
 def update():
-    global data1, ptr1
+    global data1, ptr1, xPos, yPos
     data1[:-1] = data1[1:]  # shift data in the array one sample left
                             # (see also: np.roll)
+    
+    # get GPS speed (m/s)
     speed = ir['Speed']
     if(abs(speed) < 1):
         data1[-1] = 0
         speed = 0
     else:
         data1[-1] = speed
+
+    # estimate position (m)
+    x = ir['VelocityX'] if abs(ir['VelocityX']) > 0.1 else 0
+    y = ir['VelocityY'] if abs(ir['VelocityY']) > 0.1 else 0
+    yawNorth = ir['YawNorth']
+    #print("yaw relative to North (rad): " + str(yawNorth))
+
+    deltaX = x*math.sin(yawNorth) + y*math.cos(yawNorth)
+    deltaY = x*math.cos(yawNorth) + y*math.sin(yawNorth)
+
+    xPos += deltaX * .02
+    yPos += deltaY * .02
+    arrX.append(xPos)
+    arrY.append(yPos)
+    map.clear()
+    map.plot(arrX, arrY)
     
     f = open("data.csv", "a")
     speed = str(speed)
     time = str(ir['SessionTime'])
-    laps = str(ir['RaceLaps'])
+    laps = str(ir['LapCompleted'])
     gear = str(ir['Gear'])
     brake = str(ir['Brake'])
     throttle = str(ir['Throttle'])
     steering = str(ir['SteeringWheelAngle'])
-    row = time + "," + laps + "," + speed + "," + gear + "," + brake + "," + throttle + "," + steering + "\n"
+    row = time + "," + laps + "," + speed + "," + gear + "," + brake + "," + throttle + "," + steering + "," + str(xPos) + "," + str(yPos) + "\n"
     f.write(row)
     f.close()
-    x = ir['VelocityX']
-    y = ir['VelocityY']
-    z = ir['VelocityZ']
-    print(str(x) + "\t" + str(y) + "\t" + str(z))
+
     
     ptr1 += 1
     curve2.setData(data1)
@@ -69,35 +96,5 @@ timer = pg.QtCore.QTimer()
 timer.timeout.connect(update)
 timer.start(20)
 
-# here we check if we are connected to iracing
-# so we can retrieve some data
-def check_iracing():
-    if state.ir_connected and not (ir.is_initialized and ir.is_connected):
-        state.ir_connected = False
-        # don't forget to reset your State variables
-        state.last_car_setup_tick = -1
-        # we are shutting down ir library (clearing all internal variables)
-        ir.shutdown()
-        print('irsdk disconnected')
-    elif not state.ir_connected and ir.startup() and ir.is_initialized and ir.is_connected:
-        state.ir_connected = True
-        print('irsdk connected')
-
-    
 
 pg.exec()
-try:
-    # infinite loop
-    while True:
-        # check if we are connected to iracing
-        check_iracing()
-        # if we are, then process data
-        if state.ir_connected:
-            pass
-        # sleep for 1 second
-        # maximum you can use is 1/60
-        # cause iracing updates data with 60 fps
-        time.sleep(1/60)
-except KeyboardInterrupt:
-    # press ctrl+c to exit
-    pass
